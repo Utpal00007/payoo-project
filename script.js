85eaec2509e23a6f38254fe08234c74fa37c9b28
Utpal <2022746@iub.edu.bd>
2025-11-25 12:04:25 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+ // login button functionality
+
+ document.getElementById('loginButton')
+ .addEventListener('click',function(e){
+    e.preventDefault()
+    console.log('login button clicked')
+    console.log(e)
+ })
