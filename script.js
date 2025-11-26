87df2378db18d69f5969b843fb9716d73e7aabc3
Utpal <2022746@iub.edu.bd>
2025-11-26 13:05:32 +0600

    adde home section html and css style

--- script.js
+++ script.js
@@ -3,6 +3,27 @@
  document.getElementById('loginButton')
  .addEventListener('click',function(e){
     e.preventDefault()
-    console.log('login button clicked')
-    console.log(e)
+    const mobileNumber = 1234567890 ;
+    const pinNumber = 1234;
+    const mobileNumberValue = document.getElementById
+    ('mobile-number').value //get number or get value comes in as a string 
+    // make it an integer number and value will be kept on the parseInt parenthesis
+    const mobileNumberValueConverted = parseInt (mobileNumberValue)
+
+
+    const pinNumberValue = document.getElementById
+    ('pin-number').value
+    const pinNumberValueConverted = parseInt (pinNumberValue)
+
+    console.log(mobileNumberValueConverted,pinNumberValueConverted)
+
+    if(mobileNumberValueConverted === mobileNumber && 
+      pinNumberValueConverted === pinNumber ){
+       window.location.href = './home.html'
+    } else  {
+      alert('Invalid Mobile Number or Pin')
+    }
+
+
+ 
  })
