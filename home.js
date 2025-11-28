78661f8b1d54ca809ce09a46f00522ae93e3f456
Utpal <2022746@iub.edu.bd>
2025-11-28 09:08:45 +0600

    added add money and cast out toggle feature

--- home.js
+++ home.js
@@ -1,1 +1,58 @@
-console.log('Home page')
+
+const validPin = 1234;
+
+document.getElementById("add-money-btn")
+.addEventListener('click',function(e){
+    e.preventDefault()
+    console.log("add money btn clicked ")
+
+    // for selecting bank 
+    const bank = document.getElementById('bank' ).value
+    const accountNumber = document.getElementById('account-number').value
+    const addAmount =  parseInt(document.getElementById('add-amount').value)
+    const addPin = parseInt(document.getElementById('add-pin').value)
+
+    console.log(bank,accountNumber,addAmount,addPin)
+
+    const availableBalance =  parseInt(document.getElementById('available-balance').innerText)
+
+
+
+    console.log(availableBalance)
+
+     
+    if(accountNumber.length < 11 ){
+        alert('please provide valid account number')
+        return
+    }
+
+    if( addPin !== validPin  ){
+        alert('Please provide valid pin number')
+        return
+    }
+
+    const totalNewAvailableBalance = availableBalance + addAmount
+
+    document.getElementById('available-balance').innerText = totalNewAvailableBalance
+
+
+     
+
+
+
+})
+
+
+// toggling feature
+document.getElementById('add-money')
+.addEventListener('click', function() {
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.querySelector('.add-money-parent').style.display = 'block';
+});
+
+document.getElementById('cash-out')
+.addEventListener('click', function() {
+    document.querySelector('.add-money-parent').style.display = 'none';
+    document.getElementById('cash-out-parent').style.display = 'block';
+});
+
